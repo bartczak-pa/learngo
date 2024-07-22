a1b15d2812a39e760fe19b27e92a490a1dc660ac
Pawel Bartczak <pawel@bartczak.tech>
2024-07-22 19:27:32 +0100

    Completed short declaration discard exercise

--- 06-variables/03-short-declaration/exercises/05-short-discard/main.go
+++ 06-variables/03-short-declaration/exercises/05-short-discard/main.go
@@ -8,6 +8,8 @@
 
 package main
 
+import "fmt"
+
 // ---------------------------------------------------------
 // EXERCISE: Short Discard
 //
@@ -28,9 +30,8 @@ package main
 
 func main() {
 	// ADD YOUR DECLARATIONS HERE
-	//
-
+	isLightOn, _ := true, true
 	// THEN UNCOMMENT THE CODE BELOW
 
-	// fmt.Println(on)
+	fmt.Println(isLightOn)
 }
